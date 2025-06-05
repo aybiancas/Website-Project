da4a56e6b5c30e219c6abe914803fb50998e95eb
aybiancas <biancasandu998@gmail.com>
2025-06-05 19:26:53 +0300

    Adaugat logica schimbare tema

--- resurse/js/schimba_tema.js
+++ resurse/js/schimba_tema.js
@@ -1,13 +1,23 @@
 window.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("schimba_tema").onclick = function() {
-        // document.body.classList.add("dark") // adauga clasa
-        // document.body.classList.toggle("dark") // daca este, o sterge, daca nu este, o adauga
-        // true cand o adauga, false cand e deja si o sterge
-        if(document.body.classList.toggle("dark")) {
-            localStorage.setItem("tema", "dark")
-        }
-        else {
-            localStorage.removeItem("tema")
-        }
+ 
+    const temaSelect = document.getElementById("tema-select");
+    const teme = ["dark", "albastru"];
+
+    const temaSalvata = localStorage.getItem("tema");
+    if (temaSalvata && teme.includes(temaSalvata)) {
+        document.body.classList.add(temaSalvata);
+        temaSelect.value = temaSalvata;
     }
+
+    temaSelect.addEventListener("change", function () {
+        teme.forEach(t => document.body.classList.remove(t));
+
+        const temaAleasa = temaSelect.value;
+        if (temaAleasa && teme.includes(temaAleasa)) {
+            document.body.classList.add(temaAleasa);
+            localStorage.setItem("tema", temaAleasa);
+        } else {
+            localStorage.removeItem("tema");
+        }
+    });
 })
