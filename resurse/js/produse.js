d0959157b84540d290094d534e1e48eb93d0c864
aybiancas <biancasandu998@gmail.com>
2025-06-20 17:18:59 +0300

    Adaugat sortare 2 chei

--- resurse/js/produse.js
+++ resurse/js/produse.js
@@ -1,19 +1,21 @@
-window.onload= function(){
+window.onload = function () {
     document.getElementById("mesaj-prod").style.display = 'none'
 
-    let produse= document.getElementsByClassName("produs")
-    for(let prod of produse) {
+    let produse = document.getElementsByClassName("produs")
+
+    for (let prod of produse) {
         prod.querySelector("#icon-ieftin").style.display = 'none'
     }
 
-    function caractereRom(str){
+    function caractereRom(str) {
         return str
-        .toLowerCase()
-        .replace(/[ăâ]/g, 'a')
-        .replace(/î/g, 'i')
-        .replace(/ș/g, 's')
-        .replace(/ț/g, 't');
+            .toLowerCase()
+            .replace(/[ăâ]/g, 'a')
+            .replace(/î/g, 'i')
+            .replace(/ș/g, 's')
+            .replace(/ț/g, 't');
     }
+
     let textareaDesc = document.getElementById("desc-caut");
     textareaDesc.addEventListener("input", function () {
         const val = textareaDesc.value.trim();
@@ -29,52 +31,52 @@ window.onload= function(){
         let counterProd = Array.from(produse).filter(prod => prod.style.display === 'block').length
 
         const mesajProd = document.getElementById("count-produse")
-        if(mesajProd) {
+        if (mesajProd) {
             mesajProd.textContent = `Produse afisate: ${counterProd}`
         }
     }
-    
- 
+
+
     function filtareOnChange() {
-        let inpNume= document.getElementById("inp-nume").value.trim().toLowerCase()
-        let vectRadio=document.getElementsByName("gr_rad")
-        let inpAlf=null
-        let stLitera=null
-        let drLitera=null
-        for (let rad of vectRadio){
-            if (rad.checked){
-                inpAlf=rad.value
-                if (inpAlf!="toate"){
-                    [stLitera, drLitera]=inpAlf.split("-")
+        let inpNume = document.getElementById("inp-nume").value.trim().toLowerCase()
+        let vectRadio = document.getElementsByName("gr_rad")
+        let inpAlf = null
+        let stLitera = null
+        let drLitera = null
+        for (let rad of vectRadio) {
+            if (rad.checked) {
+                inpAlf = rad.value
+                if (inpAlf != "toate") {
+                    [stLitera, drLitera] = inpAlf.split("-")
                     stLitera = stLitera.trim().toUpperCase();
                     drLitera = drLitera.trim().toUpperCase();
                 }
                 break
             }
         }
-        let inpPret= parseFloat(document.getElementById("inp-pret").value)
-        let inpCategorie= document.getElementById("inp-categorie").value.trim().toLowerCase()
+        let inpPret = parseFloat(document.getElementById("inp-pret").value)
+        let inpCategorie = document.getElementById("inp-categorie").value.trim().toLowerCase()
         let inpAn = Array.from(document.getElementById("inp-an").selectedOptions).map(opt => parseInt(opt.value.trim()))
         let inpDesc = document.getElementById("desc-caut").value.trim().toLowerCase()
         let descCuv = inpDesc ? inpDesc.split(/\s+/) : [];
         let inpISBN = document.getElementById("inp-datalist").value.trim()
 
-        let produse= document.getElementsByClassName("produs")
+        let produse = document.getElementsByClassName("produs")
         let pretMin = 10000
 
-        for (let prod of produse){
-            prod.style.display="none";
-            let nume=prod.getElementsByClassName("val-nume")[0].innerHTML.trim().toLowerCase()
-            let cond1= nume.startsWith(inpNume)
-            let autor=prod.getElementsByClassName("val-autor")[0].innerHTML.trim()
-            let primAutor=autor.split(",")[0].trim()
-            let numeFamAutor=primAutor.split(" ").pop()
-            let initiala=numeFamAutor.charAt(0).toUpperCase()
-            let cond2= (inpAlf=="toate" || (stLitera<=initiala && initiala<=drLitera) )
-            let pret=parseFloat(prod.getElementsByClassName("val-pret")[0].innerHTML.trim())
+        for (let prod of produse) {
+            prod.style.display = "none";
+            let nume = prod.getElementsByClassName("val-nume")[0].innerHTML.trim().toLowerCase()
+            let cond1 = nume.startsWith(inpNume)
+            let autor = prod.getElementsByClassName("val-autor")[0].innerHTML.trim()
+            let primAutor = autor.split(",")[0].trim()
+            let numeFamAutor = primAutor.split(" ").pop()
+            let initiala = numeFamAutor.charAt(0).toUpperCase()
+            let cond2 = (inpAlf == "toate" || (stLitera <= initiala && initiala <= drLitera))
+            let pret = parseFloat(prod.getElementsByClassName("val-pret")[0].innerHTML.trim())
             let cond3 = (inpPret <= pret)
-            let categorie=prod.getElementsByClassName("val-categorie")[0].innerHTML.trim().toLowerCase()
-            let cond4 =  (inpCategorie=="toate" || inpCategorie==categorie)
+            let categorie = prod.getElementsByClassName("val-categorie")[0].innerHTML.trim().toLowerCase()
+            let cond4 = (inpCategorie == "toate" || inpCategorie == categorie)
             let an = parseInt(prod.getElementsByClassName("val-data")[0].innerHTML.trim())
             let cond5 = inpAn.includes(an)
             let disp = prod.getElementsByClassName("val-disp")[0].innerHTML.trim().toLowerCase()
@@ -85,9 +87,9 @@ window.onload= function(){
             let cond7 = (descCuv.length === 0 || descCuv.some(cuv => descriereCuv.includes(cuv)))
             let isbn = prod.getElementsByClassName("val-isbn")[0].innerHTML.trim()
             let cond8 = (inpISBN == isbn || inpISBN == "")
-            if (cond1 && cond2 && cond3 && cond4 && cond5 && cond6 && cond7 && cond8){
-                prod.style.display="block"
-                if(pret < pretMin) {
+            if (cond1 && cond2 && cond3 && cond4 && cond5 && cond6 && cond7 && cond8) {
+                prod.style.display = "block"
+                if (pret < pretMin) {
                     pretMin = pret
                 }
             }
@@ -95,8 +97,8 @@ window.onload= function(){
 
         counterProduseAfisate()
 
-        for(let prod of produse) {
-            if(parseFloat(prod.getElementsByClassName("val-pret")[0].innerHTML.trim()) == pretMin) {
+        for (let prod of produse) {
+            if (parseFloat(prod.getElementsByClassName("val-pret")[0].innerHTML.trim()) == pretMin) {
                 prod.querySelector("#icon-ieftin").style.display = 'block'
                 // console.log("-------------------query selector------------------------------", prod.querySelector("#icon-ieftin"))
             }
@@ -111,6 +113,7 @@ window.onload= function(){
             mesajProduse.style.display = condVizibil ? 'none' : 'block';
         }
     }
+
     filtareOnChange()
     document.getElementById("inp-nume").onchange = filtareOnChange
     document.getElementById("inp-pret").onchange = filtareOnChange
@@ -123,14 +126,104 @@ window.onload= function(){
 
     document.getElementById("filtrare").onclick = filtareOnChange
 
-    document.getElementById("inp-pret").onmousemove = function(){
-        document.getElementById("infoRange").innerHTML=`(${this.value})`
+    document.getElementById("inp-pret").onmousemove = function () {
+        document.getElementById("infoRange").innerHTML = `(${this.value})`
     }
-    
-/// confirm() pentru confirmarea pt butonul de resetare din pagina
-    document.getElementById("resetare").onclick=function(){
+
+    function extragereVal(prod, val) {
+        switch (val) {
+            case "pret": return parseFloat(prod.getElementsByClassName("val-pret")[0].innerHTML.trim())
+            case "nume": return prod.getElementsByClassName("val-nume")[0].innerHTML.trim().toLowerCase()
+            case "an": return parseInt(prod.getElementsByClassName("val-data")[0].innerHTML.trim())
+        }
+    }
+
+    function compSortare(val1, val2, semn) {
+        if (typeof val1 === "number" && typeof val2 === "number") {
+            return semn * (val1 - val2)
+        }
+        else {
+            return semn * val1.localeCompare(val2)
+        }
+    }
+
+    const sortareBtn = document.getElementById("sortare-btn")
+    if (!sortareBtn) {
+        // console.error("err BUTRON SORATRE");
+        return;
+    }
+    sortareBtn.onclick = function () {
+        let cheie1 = document.getElementById("cheie1").value
+        let cheie2 = document.getElementById("cheie2").value
+        let semn = parseInt(document.getElementById("ordine").value)
+        // console.log("cheie 1 ", cheie1)
+        // console.log("cheie 2 ", cheie2)
+        let produse = document.getElementsByClassName("produs");
+        let vProduse = Array.from(produse);
+
+        vProduse.sort(function (a, b) {
+            let val1 = extragereVal(a, cheie1)
+            let val2 = extragereVal(b, cheie1)
+            // console.log("val 1 ", val1)
+            // console.log("val 2 ", val2)
+
+            let cmp1 = compSortare(val1, val2, semn)
+            console.log(cmp1)
+            if (cmp1 !== 0) {
+                return cmp1;
+            }
+
+            let val11 = extragereVal(a, cheie2)
+            let val22 = extragereVal(b, cheie2)
+            let cmp2 = compSortare(val11, val22, semn)
+            console.log(cmp2)
+            return cmp2
+        })
+
+        console.log(vProduse)
+        for (let prod of vProduse) {
+            prod.parentNode.appendChild(prod);
+        }
+    }
+
+
+    // function sorteaza(){
+    //     let cheie1 = document.getElementById("cheie1").value
+    //     let cheie2 = document.getElementById("cheie2").value
+    //     let semn = parseInt(document.getElementById("ordine").value)
+    //     console.log("cheie 1 ", cheie1)
+    //     console.log("cheie 2 ", cheie2)
+    //     let produse= document.getElementsByClassName("produs");
+    //     let vProduse= Array.from(produse);
+
+    //     vProduse.sort(function(a,b){
+    //         let val1 = extragereVal(a, cheie1)
+    //         let val2 = extragereVal(b, cheie1)
+    //         // console.log("val 1 ", val1)
+    //         // console.log("val 2 ", val2)
+
+    //         let cmp1 = compSortare(val1, val2, semn)
+    //         if(cmp1 !== 0) {
+    //             return cmp1;
+    //         }
+
+    //         let val11 = extragereVal(a, cheie2)
+    //         let val22 = extragereVal(b, cheie2)
+    //         let cmp2 = compSortare(val11, val22, semn)
+    //         return cmp2
+    //     })
+    //     for (let prod of vProduse){
+    //         prod.parentNode.appendChild(prod);
+    //     }
+    // }
+
+
+
+
+    /// confirm() pentru confirmarea pt butonul de resetare din pagina
+    document.getElementById("resetare").onclick = function () {
         var confirmareReset = confirm("Sigur doriti sa resetati toate filtrele?")
-        if(!confirmareReset) return;
+        if (!confirmareReset) return;
 
         document.getElementById("inp-nume").value = ""
         document.getElementById("i_rad4").checked = true;
@@ -140,48 +233,25 @@ window.onload= function(){
         slider.value = slider.min
         document.getElementById("infoRange").textContent = `(${slider.value})`
         let selectMult = document.getElementById("inp-an")
-        for(let i = 0; i < selectMult.options.length; i++) {
+        for (let i = 0; i < selectMult.options.length; i++) {
             selectMult.options[i].selected = true
         }
         document.getElementById("inp-datalist").value = ""
         filtareOnChange()
-        let produse= document.getElementsByClassName("produs")
-        for (let prod of produse){
+        let produse = document.getElementsByClassName("produs")
+        for (let prod of produse) {
             prod.style.display = "block";
         }
     }
-    document.getElementById("sortCrescNume").onclick=function(){
-        sorteaza(1)
-    }
-    document.getElementById("sortDescrescNume").onclick=function(){
-        sorteaza(-1)
-    }
-    function sorteaza(semn){
-        let produse= document.getElementsByClassName("produs");
-        let vProduse= Array.from(produse);
-        vProduse.sort(function(a,b){ // a si b sunt <article>
-            // sortare dupa pret
-            let pretA=parseFloat(a.getElementsByClassName("val-pret")[0].innerHTML.trim())
-            let pretB=parseFloat(b.getElementsByClassName("val-pret")[0].innerHTML.trim())
-            if(pretA != pretB) {
-                return semn * (pretA - pretB)
-            }
-            let numeA=a.getElementsByClassName("val-nume")[0].innerHTML.trim().toLowerCase()
-            let numeB=b.getElementsByClassName("val-nume")[0].innerHTML.trim().toLowerCase()
-            return semn * numeA.localeCompare(numeB)
-        })
-        for (let prod of vProduse){
-            prod.parentNode.appendChild(prod);
-        }
-    }
-    window.onkeydown = function(e) {
+
+    window.onkeydown = function (e) {
         console.log(e)
-        if(e.key == 'c' && e.altKey) {
-            let produse= document.getElementsByClassName("produs")
+        if (e.key == 'c' && e.altKey) {
+            let produse = document.getElementsByClassName("produs")
             sumaPreturi = 0;
             for (let prod of produse) {
-                if(prod.style.display != "none")  {
-                    let pret=parseFloat(prod.getElementsByClassName("val-pret")[0].innerHTML.trim())
+                if (prod.style.display != "none") {
+                    let pret = parseFloat(prod.getElementsByClassName("val-pret")[0].innerHTML.trim())
                     sumaPreturi += pret
                 }
             }
@@ -191,9 +261,9 @@ window.onload= function(){
                 pRezultat.innerHTML = sumaPreturi
                 let p = document.getElementById("p-suma")
                 p.parentElement.insertBefore(pRezultat, p.nextElementSibling)
-                setTimeout(function(){
+                setTimeout(function () {
                     let p1 = document.getElementById("suma_preturi")
-                    if(p1) {
+                    if (p1) {
                         p1.remove()
                     }
                 }, 2000)
